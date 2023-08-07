f9f6294f03eea97d94651dc3da81de227e0cd3d4
Andyy Hope <andyy.hope@anz.com>
2023-08-07 09:37:49 +0800

    initial commit

---
+++ TestTwilioLLDB/TestTwilioLLDB/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  TestTwilioLLDB
+//
+//  Created by Rob Cas on 18/7/2023.
+//
+
+import UIKit
+import TwilioDependencies
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let a = 1
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        /*
+         Breakpoint on `return true` and `po a`
+        */
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ TwilioDependencies/Sources/TwilioDependencies/Placeholder.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rob Cas on 18/7/2023.
+//
+
+import Foundation
+//import TwilioConversationsClient
+/*
+public func twilioDependency() {
+    let options = TwilioConversationsClientProperties()
+    print("hi \(options)")
+}
+*/
